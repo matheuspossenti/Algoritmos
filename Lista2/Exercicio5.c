778495bc1032e26f1128444028669048d5eb49c0
Matheus Possenti <84344863+matheuspossenti@users.noreply.github.com>
2022-05-24 10:10:01 -0300

    Add files via upload

--- Lista2/Exercicio5.c
+++ Lista2/Exercicio5.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-#include <locale.h>
 
-int main (){
-    int n,i;
-    float e,fat;
-    
-    fat=1;
-    e=1;
-    printf("Informe o numero limite:\n");
-    scanf("%d", &n);
-    
-    printf("E = 1 + ");
-    for(i=1;i<=n;i++){
-        fat *= i;
-        e += 1/(float)fat;
-        printf("1/%d! + ",i);
-    }
-    printf("\n resultado= %f",e);
-   
-    return 0;
+int main(){
+	int n, cont = 1, a;
+	float e = 1, fat = 1;
+	
+	printf("Informe o valor de N: ");
+	scanf("%i", &n);
+	while(cont <= n){
+		fat=1;
+		for(a=cont; a > 0; a--){
+			fat = fat * a;
+			printf("%i - %f: %i\n", cont, fat, a);
+		}
+		e = e + (1. / fat);
+		cont++;
+	}
+	
+	printf("%.2f", e);
 }
